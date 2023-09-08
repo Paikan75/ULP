a4b98a1a8b1afcfb1bee22c946a790136ae2107c
NicoPecina <nicolaspeci@gmail.com>
2023-09-08 17:05:21 -0300

    Clase materia

--- universidadGrupo80/src/proyectotrasversal/entidades/Alumno.java
+++ universidadGrupo80/src/proyectotrasversal/entidades/Alumno.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectotrasversal.entidades;
 
 import java.time.LocalDate;
 
-/**
- *
- * @author nicop
- */
+
 public class alumno {
     private int idAlumno;
     private String apellido;
