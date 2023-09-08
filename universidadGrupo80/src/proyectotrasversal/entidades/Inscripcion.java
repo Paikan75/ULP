525ff07c51f4e5211ed6f84ed8213085de152307
JesicaB <100101914+JesicaB@users.noreply.github.com>
2023-09-08 17:27:37 -0300

    inscripcion

--- universidadGrupo80/src/proyectotrasversal/entidades/Inscripcion.java
+++ universidadGrupo80/src/proyectotrasversal/entidades/Inscripcion.java
@@ -3,6 +3,62 @@ package proyectotrasversal.entidades;
 
 
 public class Inscripcion {
+    private int idDescripcion;
+    private alumno alumno;
+    private Materia materia;
+    private double nota;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(int idDescripcion, alumno alumno, Materia materia, double nota) {
+        this.idDescripcion = idDescripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion(alumno alumno, Materia materia, double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion(double nota) {
+        this.nota = nota;
+    }
+
+    public int getIdDescripcion() {
+        return idDescripcion;
+    }
+
+    public void setIdDescripcion(int idDescripcion) {
+        this.idDescripcion = idDescripcion;
+    }
+
+    public alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
     
     
 }
