9d73f239fc375e7745e48821a194eef6e2957b21
NicoPecina <nicolaspeci@gmail.com>
2023-09-08 17:04:05 -0300

    Clase materia

--- universidadGrupo80/src/proyectotrasversal/Entidades/Materia.java
+++ universidadGrupo80/src/proyectotrasversal/Entidades/Materia.java
@@ -1,13 +1,68 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package proyectotrasversal.Entidades;
 
-/**
- *
- * @author lacim
- */
+
 public class Materia {
+    
+    private int idMateria;
+    private String nombre;
+    private int anoMateria;
+    private boolean estado;
+
+    public Materia() {
+    }
+
+    public Materia(int idMateria, String nombre, int anoMateria, boolean estado) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anoMateria = anoMateria;
+        this.estado = estado;
+    }
+
+    public Materia(String nombre, int anoMateria, boolean estado) {
+        this.nombre = nombre;
+        this.anoMateria = anoMateria;
+        this.estado = estado;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnoMateria() {
+        return anoMateria;
+    }
+
+    public void setAnoMateria(int anoMateria) {
+        this.anoMateria = anoMateria;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", anoMateria=" + anoMateria + ", estado=" + estado + '}';
+    }
+    
+    
+    
     
 }
--- universidadGrupo80/src/proyectotrasversal/entidades/Inscripcion.java
+++ universidadGrupo80/src/proyectotrasversal/entidades/Inscripcion.java
@@ -4,4 +4,5 @@ package proyectotrasversal.entidades;
 
 public class Inscripcion {
     
+    
 }
