7d82aba3bb9dbad7cf73f13fb3c6ba8ee18cbc1a
NicoPecina <nicolaspeci@gmail.com>
2023-09-08 16:09:00 -0300

    Netbeans

---
+++ universidadGrupo80/src/proyectotrasversal/ProyectoTrasversal.java
@@ -0,0 +1,14 @@
+
+package proyectotrasversal;
+
+
+public class ProyectoTrasversal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
