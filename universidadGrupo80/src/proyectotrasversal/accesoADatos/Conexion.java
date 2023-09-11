d89fcb7a167ba9eb700ecf10cbceedcf010551ab
JesicaB <100101914+JesicaB@users.noreply.github.com>
2023-09-11 16:03:07 -0300

    conexion

--- universidadGrupo80/src/proyectotrasversal/accesoADatos/Conexion.java
+++ universidadGrupo80/src/proyectotrasversal/accesoADatos/Conexion.java
@@ -1,7 +1,19 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package proyectotrasversal.accesoADatos;
 
+import java.sql.*;
 
+/**
+ *
+ * @author lacim
+ */
 public class Conexion {
-    
+        private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "universidadulp";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+    private static Connection connection;
 }
