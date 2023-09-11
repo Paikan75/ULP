6aa536b3817ae54bdd033ef1dcc8c7feea9e235c
JesicaB <100101914+JesicaB@users.noreply.github.com>
2023-09-11 17:39:04 -0300

    alumnoData

--- universidadGrupo80/src/proyectotrasversal/ProyectoTrasversal.java
+++ universidadGrupo80/src/proyectotrasversal/ProyectoTrasversal.java
@@ -13,6 +13,7 @@ public class ProyectoTrasversal {
     public static void main(String[] args) {
         // TODO code application logic here
         Connection conex = Conexion.getConnection();
+        Alumno juan = 
     }
     
 }
---
+++ universidadGrupo80/src/proyectotrasversal/accesoADatos/AlumnoData.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectotrasversal.accesoADatos;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import proyectotrasversal.Entidades.Alumno;
+
+/**
+ *
+ * @author Jesica
+ */
+public class AlumnoData {
+    private Connection con = null;
+    
+    public AlumnoData(){
+        con = Conexion.getConnection();
+    }
+    
+    public void guardarAlumno(Alumno alumno){
+        String sql = "INSERT INTO alumno(dni, apellido, nombre, fechaNacimiento, estado)"
+                + "VALUES (?, ?, ?, ?, ?)";
+        
+        try {
+            
+            PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
+            ps.setBoolean(5, alumno.isActivo());
+            
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            
+            if(rs.next()){
+                alumno.setIdAlumno(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Alumno guardado.");
+            }
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumno.");
+        }
+       
+        
+    }
+}
