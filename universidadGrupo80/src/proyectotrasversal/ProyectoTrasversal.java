a77bbab4aa8ec07570303fdf2b8cc95e3f632024
JesicaB <100101914+JesicaB@users.noreply.github.com>
2023-09-12 15:38:29 -0300

    modificarAlumno

--- universidadGrupo80/src/proyectotrasversal/ProyectoTrasversal.java
+++ universidadGrupo80/src/proyectotrasversal/ProyectoTrasversal.java
@@ -18,11 +18,13 @@ public class ProyectoTrasversal {
         // TODO code application logic here
         Connection conex = Conexion.getConnection();
         
-        Alumno juan = new Alumno(12456876,"Flores","Roberto",LocalDate.of(2012, Month.MARCH, 15),true);
+        Alumno juan = new Alumno(1,12456876,"Flores","Juan Roberto",LocalDate.of(2012, Month.MARCH, 15),true);
         
         AlumnoData alu = new AlumnoData();
         
-        alu.guardarAlumno(juan);
+        //alu.guardarAlumno(juan);
+        
+        alu.modificarAlumno(juan);
         
     }
     
--- universidadGrupo80/src/proyectotrasversal/accesoADatos/AlumnoData.java
+++ universidadGrupo80/src/proyectotrasversal/accesoADatos/AlumnoData.java
@@ -56,6 +56,34 @@ public class AlumnoData {
             JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumno.");
         }
        
+        }
+    
+     public void modificarAlumno(Alumno alumno){
+        String sql = "UPDATE alumno SET dni=?,apellido=?,nombre=?,fechaNacimiento=?"
+                + "WHERE idAlumno=?";
+        
+        try {
+            
+            PreparedStatement ps = con.prepareStatement(sql);
+            
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
+            ps.setInt(5, alumno.getIdAlumno());
+            
+            int exito = ps.executeUpdate();
+            
+            if(exito == 1){
+                JOptionPane.showMessageDialog(null, "Alumno modificado.");
+            }
+            
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla alumno.");
+        }
+       
         
     }
 }
