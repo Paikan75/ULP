8bcfb5276b6564c47de00cfb5b379ce358fbe4f6
Paikan75 <pabloarielastete@gmail.com>
2023-09-13 16:59:28 -0300

    insc1metodo

--- universidadGrupo80/src/proyectotransversal/accesoADatos/InscripcionData.java
+++ universidadGrupo80/src/proyectotransversal/accesoADatos/InscripcionData.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyectotransversal.accesoADatos;
 
-/**
- *
- * @author lacim
- */
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import proyectotransversal.Entidades.Inscripcion;
+
 public class InscripcionData {
+
+    private Connection con = null;
+
+    public InscripcionData() {
+        con = Conexion.getConnection();
+
+    }
+
+    public void guardarInscripcion (Inscripcion insc){
+    
+        String sql = "INSERT INTO inscripcion(nota, idAlumno, idMateria) VALUES ?,?,?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+          
+            ps.setDouble(1, insc.getNota());
+            ps.setInt(2, insc.getAlumno().getIdAlumno());
+            ps.setInt(3, insc.getMateria().getIdMateria());
+            
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()){
+             insc.setIdInscripcion(rs.getInt(1));
+             JOptionPane.showMessageDialog(null,"Inscripcion creada con exito");
+            }
+           ps.close();
+            
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "Error al acceder a la Tabla Inscripcion");
+        }
+        
+        
+    }
+    
+    
+    
     
 }
